29ba63c90cbf0ae876363d1f7ee932a37e8fe3df
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-01-20 19:52:54 +0000

    Resolved Issue #6 and Published v0.0.8

---
+++ examples/txtConfig_EXAMPLE.py
@@ -0,0 +1,9 @@
+# This is a Sample Example - TXTConfigParser is still in works
+
+from configPy import TXTConfigParser
+
+importedConfigs = TXTConfigParser("sample string").getConfigurations()
+
+print("Sample TXTConfigParser Test")
+
+print(importedConfigs)
