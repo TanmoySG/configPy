da646b406def7152f054779fe07b890abe575154
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-01-21 17:38:57 +0000

    Added Unit Testing

---
+++ tests/JSONConfigParser_test.py
@@ -0,0 +1,26 @@
+import json
+import unittest
+from configPy import JSONConfigParser
+
+class JSONConfigParserTestCase(unittest.TestCase):
+
+    # Success Case - 
+    # If File exists the JSONConfigParser Object should 
+    # return the content of the Existing JSON Config File
+    def test_JSONConfigParser_successCase(self):
+        importedConfigs = JSONConfigParser(configFilePath="./sampleConfig.json").getConfigurations()
+
+        with open("./sampleConfig.json", "r") as IdealCaseObj:
+            idealCaseConfigs = json.load(IdealCaseObj)
+
+        self.assertEqual(importedConfigs, idealCaseConfigs)
+
+
+    # File Does Not Exist Case - 
+    # Raises 'FileNotFoundError' Exception when file doesn't Exist
+    def test_JSONConfigParser_fileMissingCase(self):
+        self.assertRaises(FileNotFoundError, JSONConfigParser, "./samplConfig.json" )
+
+
+if __name__ == '__main__':
+    unittest.main()
