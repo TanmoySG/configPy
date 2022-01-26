c2d00d4987a162b1677a54a8c866f5b5a8cb5b85
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-01-27 01:21:26 +0530

    Update JSONConfigParser_test.py

--- tests/JSONConfigParser_test.py
+++ tests/JSONConfigParser_test.py
@@ -21,6 +21,8 @@ class JSONConfigParserTestCase(unittest.TestCase):
     def test_JSONConfigParser_fileMissingCase(self):
         self.assertRaises(FileNotFoundError, JSONConfigParser, "./samplConfig.json" )
 
-
+    def test_Another(self):
+        self.assertEqual(True, False)
+        
 if __name__ == '__main__':
     unittest.main()
