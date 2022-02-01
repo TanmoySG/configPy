80eacd036e345d62a3c66efe0a725c94d92ae0fd
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2022-02-01 20:42:53 +0000

    Added NDJSON Test Scripts

--- tests/NDJSONConfigParser_test.py
+++ tests/NDJSONConfigParser_test.py
@@ -1,25 +1,38 @@
-import json
+from configPy import NDJSONConfigParser
 import unittest
-from configPy import JSONConfigParser
+
 
 class NDJSONConfigParserTestCase(unittest.TestCase):
 
-    # Success Case - 
-    # If File exists the JSONConfigParser Object should 
+    # Success Case -
+    # If File exists the JSONConfigParser Object should
     # return the content of the Existing JSON Config File
-    def test_JSONConfigParser_successCase(self):
-        importedConfigs = JSONConfigParser(configFilePath="./sampleConfig.json").getConfigurations()
+    def test_NDJSONConfigParser_successCase(self):
+        importedConfigs = NDJSONConfigParser(
+            configFilePath="./configFiles/sampleNDJSONConfig.ndjson").getConfigurations()
 
-        with open("./sampleConfig.json", "r") as IdealCaseObj:
-            idealCaseConfigs = json.load(IdealCaseObj)
+        idealCaseConfigs = [
+            {"env": "dev", "language": "python", "version": 2.0},
+            {"env": "stage", "language": "python", "version": 3.5},
+            {"env": "prod", "language": "python", "version": 3.9}
+        ]
 
         self.assertEqual(importedConfigs, idealCaseConfigs)
 
-
-    # File Does Not Exist Case - 
+    # File Does Not Exist Case -
     # Raises 'FileNotFoundError' Exception when file doesn't exist
-    def test_JSONConfigParser_fileMissingCase(self):
-        self.assertRaises(FileNotFoundError, JSONConfigParser, "./samplConfig.json" )
-        
+
+    def test_NDJSONConfigParser_fileMissingCase(self):
+        self.assertRaises(FileNotFoundError, NDJSONConfigParser,
+                          "./configFiles/samplJSONConfig.json")
+
+    def test_NDJSONConfigParser_KeyMappingCase(self):
+        importedConfigs = NDJSONConfigParser(configFilePath="./configFiles/sampleNDJSONConfig.ndjson").mapKeysNDJSON(mappingKeys=["dev","stage","prod"]).getConfigurations()
+
+        self.assertEqual(importedConfigs["dev"]["version"], 2.0)
+        self.assertEqual(importedConfigs["stage"]["env"], "stage")
+        self.assertEqual(importedConfigs["prod"]["language"], "python")
+
+
 if __name__ == '__main__':
     unittest.main()
